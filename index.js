fbdf4a07aa3cabc5fcbe902cf995fcc1fa0d3619
discordmania <41619492+discordmania@users.noreply.github.com>
2019-01-29 18:16:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -156,7 +156,25 @@ if (cmd ===`${prefix}kiss`) {
     })
     }
   //---------------------------------------------------------------\\//---------------------------------------------------------------\\
+if(cmd === `${prefix}stats`){
+     // Variables
+    let servers = bot.guilds.size; // Server Count
+    let users = 0; // Start of user count
+    let channels = bot.channels.size; // Channel Count
+    
+    // This goes through every guild to grab an accurate memberCount;
+    bot.guilds.map(g => users += g.memberCount);
+    
+    // Form Embed
+    const embed = new Discord.MessageEmbed()
+        .setTitle('Community Channels')
+        .addField('Servers', servers, true)
+        .addField('Users', users, true)
+        .addField('Channels', channels, true);
 
+    // Send Embed
+    message.channel.send(embed);   
+}
   //---------------------------------------------------------------\\//---------------------------------------------------------------\\    
       if(cmd === `${prefix}warn`){
       
