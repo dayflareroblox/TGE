c4cd623f136a1a184b18d6a715e6fd8ed6967149
discordmania <41619492+discordmania@users.noreply.github.com>
2019-01-29 17:13:49 +0000

    Update index.js

--- index.js
+++ index.js
@@ -50,53 +50,93 @@ bot.on("message", async message => {
 }  
  
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\
-    if(cmd === `${prefix}rps`){
-        
-    let rock2 = ["Paper! I win!", "Scissors! You win!"]
-    let rock1 = Math.floor(Math.random() * rock2.length);
-  
-    let paper2 = ["Rock! You win!", "Scissors! I win!"]
-    let paper1 = Math.floor(Math.random() * paper2.length);
-  
-    let scissors2 = ["Rock! I win!", "Paper! You win!"]
-    let scissors1 = Math.floor(Math.random() * scissors2.length);
-  
-  let rock = new Discord.RichEmbed()
-  .setAuthor("Rock, Paper, Scissors")
-  .setColor("#6b5858")
-  .addField("You Chose", `${args[0]}`)
-  .addField("I choose", rock2[rock1])
-  
-  let paper = new Discord.RichEmbed()
-  .setAuthor("Rock, Paper, Scissors")
-  .setColor("#6b5858")
-  .addField("You Chose", `${args[0]}`)
-  .addField("I choose", paper2[paper1])
-  
-  let scissors = new Discord.RichEmbed()
-  .setAuthor("Rock, Paper, Scissors")
-  .setColor("#6b5858")
-  .addField("You Chose", `${args[0]}`)
-  .addField("I choose", scissors2[scissors1])
-  
-  
-  if (message.content === "+rps rock") message.channel.send(rock)
-  if (message.content === "+rps Rock") message.channel.send(rock)
-  if (message.content === "+rps r") message.channel.send(rock)
-  
-  if (message.content === "+rps paper") message.channel.send(paper)
-  if (message.content === "+rps Paper") message.channel.send(paper)
-  if (message.content === "+rps p") message.channel.send(paper)
-  
-  if (message.content === "+rps scissors") message.channel.send(scissors)
-  if (message.content === "+rps Scissors") message.channel.send(scissors)
-  if (message.content === "+rps s") message.channel.send(scissors)
-  
-  
-  if (message.content === "+rps") message.channel.send("Options: ``Rock``, ``Paper``, ``Scissors``. *Usage: +rps <option>*")
-  }
+    if(cmd === `${prefix}verify`){
+    let verifiedRole = message.guild.roles.find(r => r.name === "Verified")
+    if (message.member.roles.has(verifiedRole.id)) return message.channel.send("You are already verified.")
+
+    function makeid() {
+        var text = "";
+        var selectFruit = ['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😲', '😝', '🤑', '🤯', '😭', '😑', '😶', '😋', '🙆', '👉', '👇', '🧠', '💼', '👮🏻', '👍🏼', '👎🏼', '🐵', '🌨', '☁️', '💧', '🎬', '🎧', '🎮', '🎲', '🏅', '🥇', '🥈', '🥉', '🏆', '🏒', '🍎', '🍫', '🍿', '🍪', '🥛', '🍽', '🍴', '🐑', '🦀', '🐔', '🐭', '🦊', '🐧', '🐞', '🌍', '🌏', '🌕', '🌖', '🌚', '🌝', '🌵', '🎄', '🌲', '☀️', '⛅️', '☔️', '🍋'];
+        text += selectFruit[Math.floor(Math.random() * selectFruit.length)];
+        text += selectFruit[Math.floor(Math.random() * selectFruit.length)];
+        text += selectFruit[Math.floor(Math.random() * selectFruit.length)];
+        text += selectFruit[Math.floor(Math.random() * selectFruit.length)];
+        return text;
+
+    }
+    const filter = m => m.author.id === message.author.id
+    const collector = message.channel.createMessageCollector(filter, {
+        max: "1",
+        time: "200000"
+    })
+    const robloxEmbed = new Discord.MessageEmbed()
+        .setColor("BLUE")
+        .setTitle("Prompt")
+        .setDescription("❓ What's your ROBLOX username?")
+        .setFooter("This prompt will cancel after 200 seconds.")
+        .setTimestamp()
+    message.channel.send(robloxEmbed)
+
+    collector.on("collect", m => {
+        if (m.content === 'cancel' || m.content === 'Cancel') {
+            message.channel.send('**Cancelled prompt.**')
+            return
+        }
+        rbx.getIdFromUsername(m.content).then(foundId => {
+            const Id = foundId
+            const newString = makeid() + makeid() + makeid() + makeid() + makeid()
+            const foundUsername = new Discord.MessageEmbed()
+                .setColor("BLUE")
+                .setTitle("Prompt")
+                .setDescription("Hello **" + m.content + "**, to verify that you are that user. Please put this in your blurb, or status. \n `" + newString + "`\n\nSay **done** when complete.\nSay **cancel** to cancel. ")
+                .setFooter("Player ID is " + foundId)
+                .setImage("https://cdn.discordapp.com/attachments/498352842818715649/515541774966587392/verify_help.png")
+                .setTimestamp()
+            message.channel.send(foundUsername)
+            message.channel.awaitMessages(mag => {
+                if (mag.content.includes('done') && mag.author.id == message.author.id) {
+                    const fetchingBlurb = new Discord.MessageEmbed()
+                        .setColor("BLUE")
+                        .setTitle("Prompt")
+                        .setDescription("Fetching your emojis, please wait as I am going to fetch it.")
+                        .setFooter("Fetching..")
+                        .setTimestamp()
+                    message.channel.send(fetchingBlurb)
+                    setTimeout(function() {
+                        rbx.getStatus(foundId).then(status => {
+                            console.log(status)
+                            rbx.getBlurb(foundId).then(blurb => {
+                                if (status.includes(newString) || blurb.includes(newString)) {
+                                    const verified = new Discord.MessageEmbed()
+                                        .setColor("GREEN")
+                                        .setTitle("Prompt")
+                                        .setDescription("You have now been verified! Please wait shortly as you are going to recieve the Verified role.")
+                                        .setFooter("Verifying..")
+                                        .setTimestamp()
+                                    message.channel.send(verified)
+                                    let role = message.guild.roles.find(r => r.name === "Verified")
+                                    message.member.roles.add(role)
+                                    console.log()
+                                    message.member.setNickname(m.content)
+                                    let rolew = message.guild.roles.find(r => r.name === "UnVerified")
+                                    message.member.roles.remove(rolew)
+                                    console.log()
+                                } else {
+                                    message.channel.send("Can not find the emojis.")
+                                }
+                            })
+                        }, 5000)
+                    })
+                } else
+                if (mag.content.includes('cancel') && mag.author.id == message.author.id) {
+                    message.channel.send('**Cancelled prompt.**')
+                    return
+                }
+            })
+        })
+    })
 }
-    
+    }
   //---------------------------------------------------------------\\//---------------------------------------------------------------\\  
       if(cmd === `${prefix}warn`){
       
