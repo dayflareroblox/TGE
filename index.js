c178fff82d1bf629ae40b57f94c6937f6107e0db
discordmania <41619492+discordmania@users.noreply.github.com>
2019-01-29 18:09:23 +0000

    Update index.js

--- index.js
+++ index.js
@@ -50,47 +50,55 @@ bot.on("message", async message => {
 }  
  
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\
- if(cmd === `${prefix}profile`){
-
-        const UserInfo = new Discord.MessageEmbed()
-
-            //All Fields are Optional Pick Any some
-
-            .setAuthor(message.author.username, message.author.avatarURL()) //Heading With Username & Their Avatar 
-            .setTitle('UserInfo')
-            .setURL('www.google.com') //Any Vaild Link
-            .setColor('RANDOM') //You Can Use HexColour Ex:- #000000
-            .setImage(message.author.avatarURL()) //Add Any Image URl || Image
-            .setThumbnail(message.author.avatarURL()) //Add Any Image URl || ThumbNail
-
-            //All Feilds Are Just Examples pick Some & add as you like
-
-            .addField('Avatar', message.author.avatar, true) //The ID of the user's avatar //Inline True or false
-            .addField('AvatarURL', message.author.avatarURL({
-                format: 'png'
-            }), true) //{options} options are Size?: 128 | 256 | 512 | 1024 | 2048, Format?: "webp" | "png" | "jpg" | "gif" //.defaultAvatarURL() A link to the user's default avatar //.displayAvatarURL() A link to the user's avatar if they have one. Otherwise a link to their default avatar will be returned
-            .addField('AvatarURL', message.author.avatarURL({
-                size: '2048'
-            }), true)
-            .addField('Bot', message.author.bot, true) //Returns True If Message Author = Bot || False If Message Author not Bot.
-            .addField('Created At', message.author.createdAt, false) //The time the user was created || .createdTimestamp - The timestamp the user was created at
-            .addField('Discrim', message.author.discriminator, true) //A discriminator/tag based on username for the user Ex:- 0001
-            .addField('DMChannel', message.author.dmChannel) //The DM between the client's user and this user || If Nothing Returns "Null"
-            .addField('ID', message.author.id) //The ID of the User/author
-            .addField('Last Message', message.author.lastMessage) //The Message object of the last message sent by the user, if one was sent
-            .addField('Last Message ID', message.author.lastMessageID) //The ID of the last message sent by the user, if one was sent
-            .addField('Presence', message.author.presence) //The presence of this user
-            .addField('Presence Status', message.author.presence.status) //The presence status of this user
-            .addField('Presence Game', message.author.presence.activity.name) //The presence Game of this user
-            .addField('Tag', message.author.tag) //The Discord "tag" for this user || Ex:- Sai Chinna#6718
-            .addField('Username', message.author.username) //The username of the user || Ex:- Sai Chinna
-            .addField('Nick Name', message.guild.member(target).displayName) //Nick Name In That (message sent) server || Define target as message Author Ex:- let target = message.author; || Add This Line in Top
-
-            .setFooter('Requested By', message.author.tag) //Change To Anything As You Wish
-            .setTimestamp() //The timestamp of this embed
-
-        message.channel.send(UserInfo);
- } 
+if (command ===`${prefix}kiss`) {
+    let kiss = [
+      "https://media1.tenor.com/images/395b565d26a74bcf6b6fc8cea50df021/tenor.gif",
+      "http://cdn.smosh.com/wp-content/uploads/ftpuploads/bloguploads/awkward-kiss-little-girl.gif",
+      "https://thumbs.gfycat.com/BasicPeskyGuillemot-max-1mb.gif",
+      "https://i.pinimg.com/originals/fe/64/e9/fe64e9e2f16ced383c0cb69e5f71722d.gif",
+      "http://25.media.tumblr.com/e7f39c316f0923710c9b12e9583455ba/tumblr_mj7yrrtFaa1s7cfr2o1_500.gif",
+      "https://media2.giphy.com/media/TkDX9bkIROf8k/giphy.gif",
+      "http://gifimage.net/wp-content/uploads/2017/09/anime-gif-kiss-11.gif",
+      "https://i.imgur.com/eisk88U.gif",
+      "https://i.pinimg.com/originals/42/c3/85/42c3851fc31dc3434dfe5fa7e3463f1d.gif",
+      "https://i.makeagif.com/media/7-05-2015/553Vsb.gif",
+      "https://i.gifer.com/2II9.gif",
+      "http:/http://gif-finder.com/wp-content/uploads/2015/09/Angry-Birds-Orange-Kiss.gif/a.fod4.com/images/GifGuide/michael_scott/119477_o.gif",
+      "http://gif-finder.com/wp-content/uploads/2015/09/Angry-Birds-Orange-Kiss.gif",
+      "https://media1.tenor.com/images/6af13a438013667a81031dde8d6b6931/tenor.gif",
+      "https://media1.tenor.com/images/a3e63e98f0344a2e9a040ea5df3769b0/tenor.gif",
+      "https://media1.tenor.com/images/fb92d0be78a1ea19af0168c0ca29c1bd/tenor.gif?itemid=5615952",
+      
+    ]
+    let hugresult = Math.floor((Math.random() * kiss.length));
+    if (!args[0]) {
+        const ghembed = new Discord.RichEmbed()
+            .setColor(`RANDOM`)
+            .setTitle(`${message.author.username} kiss themself...! (weirdo)`)
+            .setImage('https://cdn.discordapp.com/attachments/452115003659780096/460369555823525898/kiss.gif')
+        message.channel.send({
+            embed: ghembed
+        })
+        return;
+    }
+    if (!message.mentions.members.first().user.username === message.isMentioned(message.author)) {
+        const hembed = new Discord.RichEmbed()
+            .setColor(`RANDOM`)
+            .setTitle(`${message.author.username} gave ${message.mentions.members.first().user.username} a hug! How sweet!`)
+            .setImage(kiss[hugresult])
+        message.channel.send({
+            embed: hembed
+        })
+        return;
+    }
+    const ghembed = new Discord.RichEmbed()
+        .setColor(`RANDOM`)
+        .setTitle(`${message.author.username} hugged themself...! (weirdo)`)
+        .setImage('https://cdn.discordapp.com/attachments/452115003659780096/460369555823525898/kiss.gif')
+    message.channel.send({
+        embed: ghembed
+    })
+}  
 
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\    
     if(cmd === `${prefix}hug`){
