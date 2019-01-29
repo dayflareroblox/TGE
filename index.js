9b6eebc1ae39ffc0a98d51113d801ce50d44dff4
discordmania <41619492+discordmania@users.noreply.github.com>
2019-01-29 20:45:08 +0000

    Update index.js

--- index.js
+++ index.js
@@ -98,7 +98,34 @@ if (cmd ===`${prefix}kiss`) {
     })
 }  
 
- //---------------------------------------------------------------\\//---------------------------------------------------------------\\    
+ //---------------------------------------------------------------\\//---------------------------------------------------------------\\
+    if(cmd === `${prefix}8ball`){
+     // Array of responses
+    let responses = [
+        'Agreed!',
+        'Of Course!',
+        'Nope.',
+        'No',
+        'Maybe',
+        'One day...',
+        'Don\'t see that happening!',
+        'I have no idea'
+    ]
+    
+    // Fetch a random item from the array
+    let fetched = responses[Math.floor(Math.random() * responses.length)];
+    
+    // Form Embed
+    const embed = new Discord.MessageEmbed()
+        .setColor(0xffffff)
+        .setFooter(fetched);
+    
+    // Send Embed
+    message.channel.send(embed);
+    }    
+
+ //---------------------------------------------------------------\\//---------------------------------------------------------------\\
+    
     if(cmd === `${prefix}hug`){
         let hug = [
         "https://data.whicdn.com/images/221692186/original.gif",
