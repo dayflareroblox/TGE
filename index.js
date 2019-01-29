7dbd851e524384bdb0f509c80b961071951283e6
discordmania <41619492+discordmania@users.noreply.github.com>
2019-01-29 17:25:35 +0000

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,6 @@
 
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
-const rbx = require("roblox-js")
 
 const bot = new Discord.Client({disableEveryone: false});
 
@@ -51,92 +50,60 @@ bot.on("message", async message => {
 }  
  
  //---------------------------------------------------------------\\//---------------------------------------------------------------\\
-    if(cmd === `${prefix}verify`){
-    let verifiedRole = message.guild.roles.find(r => r.name === "Verified")
-    if (message.member.roles.has(verifiedRole.id)) return message.channel.send("You are already verified.")
-
-    function makeid() {
-        var text = "";
-        var selectFruit = ['😀', '😁', '😂', '🤣', '😃', '😄', '😅', '😆', '😉', '😲', '😝', '🤑', '🤯', '😭', '😑', '😶', '😋', '🙆', '👉', '👇', '🧠', '💼', '👮🏻', '👍🏼', '👎🏼', '🐵', '🌨', '☁️', '💧', '🎬', '🎧', '🎮', '🎲', '🏅', '🥇', '🥈', '🥉', '🏆', '🏒', '🍎', '🍫', '🍿', '🍪', '🥛', '🍽', '🍴', '🐑', '🦀', '🐔', '🐭', '🦊', '🐧', '🐞', '🌍', '🌏', '🌕', '🌖', '🌚', '🌝', '🌵', '🎄', '🌲', '☀️', '⛅️', '☔️', '🍋'];
-        text += selectFruit[Math.floor(Math.random() * selectFruit.length)];
-        text += selectFruit[Math.floor(Math.random() * selectFruit.length)];
-        text += selectFruit[Math.floor(Math.random() * selectFruit.length)];
-        text += selectFruit[Math.floor(Math.random() * selectFruit.length)];
-        return text;
-
-    }
-    const filter = m => m.author.id === message.author.id
-    const collector = message.channel.createMessageCollector(filter, {
-        max: "1",
-        time: "200000"
-    })
-    const robloxEmbed = new Discord.MessageEmbed()
-        .setColor("BLUE")
-        .setTitle("Prompt")
-        .setDescription("❓ What's your ROBLOX username?")
-        .setFooter("This prompt will cancel after 200 seconds.")
-        .setTimestamp()
-    message.channel.send(robloxEmbed)
-
-    collector.on("collect", m => {
-        if (m.content === 'cancel' || m.content === 'Cancel') {
-            message.channel.send('**Cancelled prompt.**')
-            return
-        }
-        rbx.getIdFromUsername(m.content).then(foundId => {
-            const Id = foundId
-            const newString = makeid() + makeid() + makeid() + makeid() + makeid()
-            const foundUsername = new Discord.MessageEmbed()
-                .setColor("BLUE")
-                .setTitle("Prompt")
-                .setDescription("Hello **" + m.content + "**, to verify that you are that user. Please put this in your blurb, or status. \n `" + newString + "`\n\nSay **done** when complete.\nSay **cancel** to cancel. ")
-                .setFooter("Player ID is " + foundId)
-                .setImage("https://cdn.discordapp.com/attachments/498352842818715649/515541774966587392/verify_help.png")
-                .setTimestamp()
-            message.channel.send(foundUsername)
-            message.channel.awaitMessages(mag => {
-                if (mag.content.includes('done') && mag.author.id == message.author.id) {
-                    const fetchingBlurb = new Discord.MessageEmbed()
-                        .setColor("BLUE")
-                        .setTitle("Prompt")
-                        .setDescription("Fetching your emojis, please wait as I am going to fetch it.")
-                        .setFooter("Fetching..")
-                        .setTimestamp()
-                    message.channel.send(fetchingBlurb)
-                    setTimeout(function() {
-                        rbx.getStatus(foundId).then(status => {
-                            console.log(status)
-                            rbx.getBlurb(foundId).then(blurb => {
-                                if (status.includes(newString) || blurb.includes(newString)) {
-                                    const verified = new Discord.MessageEmbed()
-                                        .setColor("GREEN")
-                                        .setTitle("Prompt")
-                                        .setDescription("You have now been verified! Please wait shortly as you are going to recieve the Verified role.")
-                                        .setFooter("Verifying..")
-                                        .setTimestamp()
-                                    message.channel.send(verified)
-                                    let role = message.guild.roles.find(r => r.name === "Verified")
-                                    message.member.roles.add(role)
-                                    console.log()
-                                    message.member.setNickname(m.content)
-                                    let rolew = message.guild.roles.find(r => r.name === "UnVerified")
-                                    message.member.roles.remove(rolew)
-                                    console.log()
-                                } else {
-                                    message.channel.send("Can not find the emojis.")
-                                }
-                            })
-                        }, 5000)
-                    })
-                } else
-                if (mag.content.includes('cancel') && mag.author.id == message.author.id) {
-                    message.channel.send('**Cancelled prompt.**')
-                    return
-                }
-            })
+    if(cmd === `${prefix}hug`){
+        let hug = [
+        "https://data.whicdn.com/images/221692186/original.gif",
+        "http://mrwgifs.com/wp-content/uploads/2013/04/Ouran-High-School-Host-Club-Love-Hug-Gif.gif",
+        "http://images6.fanpop.com/image/photos/33100000/Kyoya-and-Tamaki-ouran-high-school-host-club-33132917-500-375.gif",
+        "http://31.media.tumblr.com/4d6525e7b5e546cde555bf2453563335/tumblr_mskyp8XJcb1r40gm7o1_1280.gif",
+        "https://i.pinimg.com/originals/34/dc/98/34dc98f17fd5cf558611f14ff9a0c1c9.gif",
+        "https://78.media.tumblr.com/6bef64140dfefe6fe86089c6eb11fb9b/tumblr_ohhnjyDJll1vm2xpgo1_500.gif",
+        "https://78.media.tumblr.com/806c23dbcf9bde033e708c8679c63975/tumblr_inline_ohhtig3BpF1rz9r19_540.gif",
+        "https://i.pinimg.com/originals/0f/48/1b/0f481bfc59229ce8127f2aba52bb8f4a.gif",
+        "https://pa1.narvii.com/6276/4461c2a865973bddcc5f4e591a165e09275c7a2c_hq.gif",
+        "https://78.media.tumblr.com/7e29c1e560c527de00a9f57bb7d941c3/tumblr_inline_ohi8745BbI1u9qbij_540.gif",
+        "https://data.whicdn.com/images/271163043/original.gif",
+        "https://78.media.tumblr.com/d00aba2e25ac11a11d9c5a770275dfc8/tumblr_orpdyc83FN1rtwid9o1_500.gif",
+        "http://0.media.dorkly.cvcdn.com/33/43/cac85de1cfd2bc4e7bec36631b260156.gif",
+        "https://i.pinimg.com/originals/22/8a/c9/228ac960b7c24ffb87374857fa6a0920.gif",
+        "https://pa1.narvii.com/6333/8c254b88d099c03be84769075ecac875c5dbb4bb_hq.gif",
+        "https://pa1.narvii.com/6449/c5383d0a548987d69aac06e8dc9b270219159b3f_hq.gif",
+        "https://media1.tenor.com/images/100c453c2f411189b40e6931ff65a88b/tenor.gif?itemid=7210521",
+        "https://i.pinimg.com/originals/e5/0e/c8/e50ec889ef64432e5d4648370014d272.gif",
+        "https://78.media.tumblr.com/94f62f2fbca608f70a48e6c04c2dc27c/tumblr_orotkrEC4t1vbbkedo2_540.gif",
+        "http://i0.kym-cdn.com/photos/images/original/001/266/481/075.gif",
+        "https://data.whicdn.com/images/310192271/original.gif",
+        "https://78.media.tumblr.com/064596e2fb0101675b89d79ac41141e0/tumblr_p8g2jmxCLD1qc9mvbo1_540.gif",
+    ]
+    let hugresult = Math.floor((Math.random() * hug.length));
+    if (!args[0]) {
+        const ghembed = new Discord.RichEmbed()
+            .setColor(0xFF0000)
+            .setTitle(`${message.author.username} hugged themself...! (weirdo)`)
+            .setImage('https://media3.giphy.com/media/ArLxZ4PebH2Ug/giphy.gif')
+        message.channel.send({
+            embed: ghembed
         })
+        return;
+    }
+    if (!message.mentions.members.first().user.username === message.isMentioned(message.author)) {
+        const hembed = new Discord.RichEmbed()
+            .setColor(0xFF0000)
+            .setTitle(`${message.author.username} gave ${message.mentions.members.first().user.username} a hug! How sweet!`)
+            .setImage(hug[hugresult])
+        message.channel.send({
+            embed: hembed
+        })
+        return;
+    }
+    const ghembed = new Discord.RichEmbed()
+        .setColor(0xFF0000)
+        .setTitle(`${message.author.username} hugged themself...! (weirdo)`)
+        .setImage('https://media3.giphy.com/media/ArLxZ4PebH2Ug/giphy.gif')
+    message.channel.send({
+        embed: ghembed
     })
-}
+    }
   //---------------------------------------------------------------\\//---------------------------------------------------------------\\  
       if(cmd === `${prefix}warn`){
       
